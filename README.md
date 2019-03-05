a903576724ee69b65b82d221cf209e4a77d95ca2
happy-yuxuan <victor.wuy@icloud.com>
2019-03-05 15:11:36 +0800

    Add gitignore

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Leetcode
 
-## Leetcode Algorithm
+### Leetcode Algorithm
 
 | **#** | **Title** | **Language** | **Difficulty** | **Main Tag** |
 | --- | --- | --- | --- | --- |
-| Wait | Wait | Wait | Wait | Wait |
+| ... | ... | ... | ... | ... |
 | 1 | [Two Sum](https://leetcode-cn.com/problems/two-sum/) | C++ | Easy | Array |
---
+++ algorithms/cpp/283.move-zeroes.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=283 lang=cpp
+ *
+ * [283] Move Zeroes
+ *
+ * https://leetcode.com/problems/move-zeroes/description/
+ *
+ * algorithms
+ * Easy (53.63%)
+ * Total Accepted:    424.4K
+ * Total Submissions: 791.2K
+ * Testcase Example:  '[0,1,0,3,12]'
+ *
+ * Given an array nums, write a function to move all 0's to the end of it while
+ * maintaining the relative order of the non-zero elements.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * 
+ * Note:
+ * 
+ * 
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ * 
+ */
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0, j = 0;   // i代表非0元素的最后位置+1， j是遍历指针。 最后[i, j)的元素都为0
+        for (; j < nums.size(); ++j) {
+            if (nums[j] != 0) {
+                nums[i++] = nums[j];
+            }
+        }
+
+        // 这个是有i指向的元素是所有非零元素的后一个，所以把[i, j)设置为0
+        for (; i < j; ++i) {
+            nums[i] = 0;
+        }
+    }
+};
+
