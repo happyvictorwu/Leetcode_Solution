49ccc340abae8ebb0be46639d343e22dbd68935d
Victor-Lab <yalaopuhuanggu@163.com>
2019-03-12 21:15:06 +0800

    Accepted 75题

--- README.md
+++ README.md
@@ -9,5 +9,6 @@
 | 283 | [Move Zeroes](https://leetcode-cn.com/problems/move-zeroes/) | [C++](./algorithms/cpp/283.move-zeroes/Solution.cpp) | Easy | Array |
 | 88 | [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/description/) | [C++](./algorithms/cpp/88.merge-sorted-array/Solution.cpp) | Easy | Array |
 | 80 | [Remove Duplicates from Sorted Array II](https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/) | [C++](./algorithms/cpp/80.remove-duplicates-from-sorted-array-ii/Solution.cpp) | Medium | Array |
+| 75 | [Sort Colors](https://leetcode.com/problems/sort-colors/description/) | [C++](./algorithms/cpp/75.sort-colors/Solution.cpp) | Medium | Array |
 | 27 | [Remove Element](https://leetcode.com/problems/remove-element/description/) | [C++](./algorithms/cpp/27.remove-element/Solution.cpp) | Easy | Array |
 | 26 | [Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/) | [C++](./algorithms/cpp/26.remove-duplicates-from-sorted-array/Solution.cpp) | Easy | Array |
---
+++ algorithms/cpp/75.sort-colors/Solution.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=75 lang=cpp
+ *
+ * [75] Sort Colors
+ *
+ * https://leetcode.com/problems/sort-colors/description/
+ *
+ * algorithms
+ * Medium (41.42%)
+ * Total Accepted:    295.2K
+ * Total Submissions: 712.6K
+ * Testcase Example:  '[2,0,2,1,1,0]'
+ *
+ * Given an array with n objects colored red, white or blue, sort them in-place
+ * so that objects of the same color are adjacent, with the colors in the order
+ * red, white and blue.
+ * 
+ * Here, we will use the integers 0, 1, and 2 to represent the color red,
+ * white, and blue respectively.
+ * 
+ * Note: You are not suppose to use the library's sort function for this
+ * problem.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ * 
+ * Follow up:
+ * 
+ * 
+ * A rather straight forward solution is a two-pass algorithm using counting
+ * sort.
+ * First, iterate the array counting number of 0's, 1's, and 2's, then
+ * overwrite array with total number of 0's, then 1's and followed by 2's.
+ * Could you come up with a one-pass algorithm using only constant space?
+ * 
+ * 
+ */
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count[3] = {0};
+        for (int i = 0; i < nums.size(); ++i) {
+            ++count[nums[i]];
+        }
+
+        int k = 0;
+        for (int i = 0; i < count[0]; ++i) {
+            nums[k++] = 0;
+        }
+
+        for (int i = 0; i < count[1]; ++i) {
+            nums[k++] = 1;
+        }
+
+        for (int i = 0; i < count[2]; ++i) {
+            nums[k++] = 2;
+        }
+    }
+};
+
